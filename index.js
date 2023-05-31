7ecf820bf7578bc5458a97df5304e3920616d9dd
Mathimugan <68652175+Mathimugan@users.noreply.github.com>
2023-05-31 10:54:17 +0800

    initial commit

--- index.js
+++ index.js
@@ -1,16 +1,49 @@
 var express = require('express');
 var app = express();
+require("dotenv").config();
 var bodyParser = require('body-parser');
 var jsonParser = bodyParser.json();
+const { v4: uuidv4 } = require('uuid');
 var urlencodedParser = bodyParser.urlencoded({ extended: false });
+const AWS = require('aws-sdk');
+require('aws-sdk/lib/maintenance_mode_message').suppress = true;
+AWS.config.update({ region: 'ap-southeast-1',accessKeyId: "AKIAVXCHOFVI2OSH7Z4C",
+secretAccessKey:"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"});
+const client = new AWS.DynamoDB.DocumentClient();
+const tableName = 'employee';
+var params = {
+    TableName: tableName
+};
 app.get('/',(req,res)=>
 {
 res.send("Hello World");
 });
 app.post('/api/addemployee', jsonParser, function (req, res) {
-    // create user in req.body
-    console.log(req.body);
-    res.send(req.body);
-  })
+    var body = req.body;
+    console.log(uuidv4());
+    var params = {
+        TableName: tableName,
+        Item: {
+            // creates a new uuid
+            "Id": uuidv4(),
+            // name property passed from body
+            "FirstName": body["firstname"],
+            "LastName": body["lastname"]
+
+        }
+    };
+
+
+    client.put(params, (err, data) => {
+        if (err) {
+            console.error("Unable to add item.");
+            console.error("Error JSON:", JSON.stringify(err, null, 2));
+        } else {
+            console.log("Added item:", JSON.stringify(data, null, 2));
+        }
+    });
+  });
+
+  
 
 app.listen(3000);
